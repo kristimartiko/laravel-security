c6ee5bd2d6bf42120210adba8e3b42eb7aa866af
Kristi <kristi.martiko@fshnstudent.info>
2021-04-16 00:05:31 +0200

    Employee/Auth

--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/register', [LoginController::class, 'register']);
 
-Route::get('/employee', [EmployeeController::class, 'index']);
+Route::middleware('auth')->get('/employee', [EmployeeController::class, 'index']);
